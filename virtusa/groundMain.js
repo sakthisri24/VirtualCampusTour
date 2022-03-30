ab830fca5871505e5e7324f7fdb292563353e98d
sakthisri24 <89906453+sakthisri24@users.noreply.github.com>
2022-03-30 23:58:16 +0530

    Add files via upload

---
+++ virtusa/groundMain.js
@@ -0,0 +1,5 @@
+pannellum.viewer('panorama-360-view', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/wJQdB1H/PANO-20220326-135118-9.jpg",
+    "autoLoad": true
+})
---
+++ virtusa/main1.js
@@ -0,0 +1,5 @@
+pannellum.viewer('panorama-360-view', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/mSf7Cq1/Classroom.jpg",
+    "autoLoad": true
+})
